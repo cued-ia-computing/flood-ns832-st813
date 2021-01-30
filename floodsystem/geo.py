f28efcd2c6d63708bdfa290e53ab6f8584b1456f
ShuohanT <cassiusthegreat11@gmail.com>
2021-01-30 00:01:45 +0000

    Complete Task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,16 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from floodsystem import station
+from haversine import haversine
+def stations_by_distance(stations, p):
+    """returns a list of station-distance tuples sorted by distance"""
+    station_distance = []
+    """Initiate the returned list"""
+    for instances in stations:
+        """Iterate the stations list"""
+        station_distance.append((instances.name, haversine(instances.coord, p)))
+        """Append the tuples of station name and distance to the list"""
+    station_distance_sorted = sorted_by_key(station_distance, 1)
+    """Sort the list by the second key, which is the distance"""
+    return station_distance_sorted
