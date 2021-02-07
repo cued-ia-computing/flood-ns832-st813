44f621425de492820a778cf34508bb4229d0a991
Naomi Saad <ns832@cam.ac.uk>
2021-02-07 16:21:47 +0000

    Task 1E and 1F (from previous files due to issues)

---
+++ Task1F.py
@@ -0,0 +1,9 @@
+from floodsystem import stationdata
+from floodsystem.geo import inconsistent_typical_range_stations
+
+def run():
+    stat = inconsistent_typical_range_stations(stationdata.build_station_list())
+    print (stat)
+
+if __name__ == "__main__":
+    run()
